a13418bee2aa5ae15aa724568c92fb95775c25e4
Dmitry <mitrybox@live.com>
2019-05-15 14:51:04 +0300

    Add the textBox1_TextChanged

--- testRep/Form1.Designer.cs
+++ testRep/Form1.Designer.cs
@@ -29,20 +29,33 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.textBox1.Location = new System.Drawing.Point(43, 43);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(531, 178);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -54,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- testRep/Form1.cs
+++ testRep/Form1.cs
@@ -15,16 +15,31 @@ namespace testRep
         public Form1()
         {
             InitializeComponent();
-            AutoCompleteStringCollection source = new AutoCompleteStringCollection()
-        {
-            "A",
-            "B",
-            "C",
-            "D"
-        };
-            textBox1.AutoCompleteCustomSource = source;
-            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
-            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+            textBox1.TextChanged += textBox1_TextChanged;
+            
+                AutoCompleteStringCollection source = new AutoCompleteStringCollection()
+            {
+                "Aaaaa",
+                "Bbbbb",
+                "Ccccc",
+                "Ddddd"
+            };
+                textBox1.AutoCompleteCustomSource = source;
+                textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            }
+
+            private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+            
+            private void textBox1_TextChanged(object sender, EventArgs e)
+            {
+                label1.Text = textBox1.Text;
+            }
+
+
         }
-    }
 }
